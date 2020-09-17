a284aa55b83a03f20a7e816a311b5b08ea37decb
fake-doc <71298541+fake-doc@users.noreply.github.com>
2020-09-16 22:10:36 -0400

    Add files via upload

---
+++ one2OneOrNah.py
@@ -0,0 +1,30 @@
+
+
+##Parameters: Start,stop : The domain on a closed inteveral - defined by the set of integers
+            ##a,b,c the values for the parabola ax^2+bx+c - defined by the set of integers
+            ##Check to see if a parabola within a certain ibteger domain is one -to-one
+
+
+def one2OneOrNah(start,stop,a,b,c):
+    x = start
+    y=start
+    z=0
+    i=start
+    isOne2One = True
+    while i<=stop and isOne2One:
+        outputA =a*i**2 + b*i +c 
+        x=y+z
+        z+=1
+        while x<=stop: 
+            outputB = a*x**2 + b*x +c
+            
+            if outputA==outputB and x!=i:
+                isOne2One= False
+            x+=1
+            #print("(" + str(i),str(outputA) + "),("+ str(x-1),str(outputB) +")")
+        i+=1
+    return isOne2One
+
+
+yeet = one2OneOrNah(-1,1,1,0,0)
+print(yeet)
